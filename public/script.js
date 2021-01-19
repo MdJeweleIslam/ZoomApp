15fdddaabfd99a424aa93fbd1a8c5fc89631389e
Md Jewele Islam <mdjeweleislam@gmail.com>
2021-01-19 11:46:35 +0600

    create functionality for User connection and disconnection

--- public/script.js
+++ public/script.js
@@ -11,12 +11,38 @@ const MyPeer = new Peer(undefined, {
 const Myvideo = document.createElement('video')
 Myvideo.muted = true;
 
+const peers = {}
 
 navigator.mediaDevices.getUserMedia({
 	video: true,
 	audio: true
 }).then(stream => {
 	addVideoStream(Myvideo, stream)
+
+	MyPeer.on('call', () => {
+		call.answer(stream)
+		const video = document.createElement('vedio')
+		call.on('stream', userVideoStream => {
+			addVideoStream(video, userVideoStream)
+		})
+	})
+
+	MyPeer.on('call', call => {
+		call.answer(stream)
+		const video = document.createElement('video')
+
+		call.on('stream', userVideoStream => {
+			addVideoStream(video, userVideoStream)
+		})
+	})
+
+	socket.on('user-connected', userId => {
+		connectToNewUser(userId, stream)
+	})
+})
+
+socket.on('user-disconnected', userId => {
+	if(peers[userId]) peers[userId].close()
 })
 
 
@@ -28,10 +54,27 @@ socket.on('user-connected', userId => {
 	console.log('User Connected: ' + userId)
 })
 
+socket.on('user-disconnected', userId => {
+	console.log(userId)
+})
+
 function addVideoStream(video, stream){
 	video.srcObject = stream
 	video.addEventListener('loadedmetadata', () => {
 		video.play()
 	})
 	videGrid.append(video)
+}
+
+function connectToNewUser(userId, stream){
+	const call = MyPeer.call(userId, stream)
+	const video = document.createElement('video')
+	call.on('stream', userVideoStream => {
+		addVideoStream(video, userVideoStream)
+	})
+	call.on('close', () => {
+		video.remove()
+	})
+
+	peers[userId] = call
 }
